ff7153f43f245411f4849b8d8499a395ce91f0b4
bsprink <bsprink@comcast.net>
2017-02-04 20:28:17 -0600

    got jquery working on primary

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
+//My Scripts//
+//$("#blueViolet").mouseenter(function(){
+	//$("#blueViolet").css("fill","black");
+//});
+//$("#blueViolet").mouseleave(function(){
+	//$("#blueViolet").css("fill","blueViolet");
+//});
 $("#pribtn").click(function(){
-	$("#wheelSVG path").css("opacity",".5");
-}
+	$("#wheelSVG path").css("opacity",".1");
+	$("#blue").css("opacity","1");
+	$("#yellow").css("opacity","1");
+	$("#red").css("opacity","1");
+});
