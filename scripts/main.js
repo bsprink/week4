ece3a98b178a224ad4b042e4710acab16f3e47fc
bsprink <bsprink@comcast.net>
2017-02-05 17:09:27 -0600

    three buttons working

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,51 @@
 //$("#blueViolet").mouseleave(function(){
 	//$("#blueViolet").css("fill","blueViolet");
 //});
+var selectedColor;
+
 $("#pribtn").click(function(){
 	$("#wheelSVG path").css("opacity",".1");
 	$("#blue").css("opacity","1");
 	$("#yellow").css("opacity","1");
 	$("#red").css("opacity","1");
 });
+
+$("#secbtn").click(function(){
+	$("#wheelSVG path").css("opacity",".1");
+	$("#green").css("opacity","1");
+	$("#violet").css("opacity","1");
+	$("#orange").css("opacity","1");
+});
+
+$("#terbtn").click(function(){
+	$("#wheelSVG path").css("opacity",".1");
+	$("#blueViolet").css("opacity","1");
+	$("#yellowOrange").css("opacity","1");
+	$("#redViolet").css("opacity","1");
+	$("#blueGreen").css("opacity","1");
+	$("#yellowGreen").css("opacity","1");
+	$("#redOrange").css("opacity","1");
+});
+
+$("#blue").click(function(){
+	$("#text").css("color","blue");
+	$("#text").html("blue");
+	selectedColor = "b";
+});
+
+$("#compbtn").click(function(){
+	if(selectedColor == "b"){
+		$("#wheelSVG path").css("opacity",".1");
+		$("#blue").css("opacity","1");
+		$("#orange").css("opacity","1");
+		
+	}
+
+});
+
+
+//$("#compbtn").click(function(){
+	//if(selectedColor == "blue"){
+
+	//}
+//});
